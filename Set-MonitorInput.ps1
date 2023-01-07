30a1f47d50dcbd55db8de8ba10322d934785e4fb
TheRealZero <lucasallman@gmail.com>
2023-01-07 14:40:14 -0500

    Committing monitor switching script.

---
+++ Set-MonitorInput.ps1
@@ -0,0 +1,8 @@
+Function Set-HDMI1{
+
+    C:\Users\lucas\Downloads\ControlMyMonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 18
+}
+
+Function Set-HDMI2{
+    C:\Users\lucas\Downloads\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 17
+}
