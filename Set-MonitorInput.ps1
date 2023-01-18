b5f08c368590a0e561fe552622588c5bb0e897b7
TheRealZero <lucasallman@gmail.com>
2023-01-18 04:31:32 -0500

    working monitor api script, added scheduled task export

--- Set-MonitorInput.ps1
+++ Set-MonitorInput.ps1
@@ -1,7 +1,7 @@
 param(
     $inputChoice
 )
-Function Set-HDMI1{# Laptops
+Function Set-HDMI2{# Laptops
 
     C:\Windows\system32\DisplaySwitch.exe 2
     C:\Users\lucas\Downloads\ControlMyMonitor\ControlMyMonitor.exe /SetValue "2367" D6 1
@@ -9,7 +9,7 @@ Function Set-HDMI1{# Laptops
     C:\Users\lucas\Downloads\ControlMyMonitor\ControlMyMonitor.exe /SetValue "2367" 60 18
 }
 
-Function Set-HDMI2{# Desktop
+Function Set-HDMI1{# Desktop
     C:\Windows\system32\DisplaySwitch.exe 3
     C:\Users\lucas\Downloads\ControlMyMonitor\ControlMyMonitor.exe /SetValue "2367" D6 1
     Start-Sleep -Seconds 2
@@ -22,10 +22,10 @@ Function Set-VGA{
 Function Get-CurrentInput{
     $cv = C:\Users\lucas\Downloads\ControlMyMonitor\ControlMyMonitor.exe /GetValue "2367" 60
     if($cv -eq 18){
-        Write-Output "HDMI1"
+        Write-Output "HDMI2"
     }
     elseif($cv -eq 17){
-        Write-Output "HDMI2"
+        Write-Output "HDMI1"
     }
     elseif($cv -eq 1){
         Write-Output "VGA"
@@ -35,11 +35,11 @@ Function Get-CurrentInput{
     }
 }
 
-if ($inputChoice -eq "HDMI1" -OR $inputChoice -eq "Laptop" ){
-    Set-HDMI1
-}
-elseif ($inputChoice -eq "HDMI2" -OR $inputChoice -eq "Desktop"){
+if ($inputChoice -eq "HDMI2" -OR $inputChoice -eq "Laptop" ){
     Set-HDMI2
+}
+elseif ($inputChoice -eq "HDMI1" -OR $inputChoice -eq "Desktop"){
+    Set-HDMI1
 }
 elseif ($inputChoice -eq "VGA"){
     Set-VGA
@@ -48,7 +48,7 @@ else {
     Get-CurrentInput
 }
 
-<# AOC
+<# AOCc:\g
 Monitor Device Name: "\\.\DISPLAY2\Monitor0"
 Monitor Name: "2367"
 Serial Number: "BHKD99A002849"
